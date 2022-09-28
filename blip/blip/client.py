b7daa271fda43b5fa781d39e585a2951c55fba18
Jonathan Tavares <jonathantavares.all@gmail.com>
2022-09-28 19:40:15 -0300

    feat: desisti de usar o blip

--- blip/blip/client.py
+++ blip/blip/client.py
@@ -3,13 +3,14 @@ from uuid import uuid4
 
 class BlipClient:
     def __init__(self, cellphone, api_key:str, contract:str, start_message='bliptester'):
+        contract = contract+".http." if contract != '' else ''
         self.namespace = 'aa3ec985_9c33_4b56_8c1a_0449dc92104a'
         self.start_message = start_message
         self.identity = f'{cellphone}@wa.gw.msging.net'
         self.cellphone = cellphone
         self.headers = {'Content-Type': 'application/json', 'Authorization': api_key}
-        self.command_url = f'https://{contract}.http.msging.net/commands'
-        self.message_url = f'https://{contract}.http.msging.net/messages'
+        self.command_url = f'https://{contract}msging.net/commands'
+        self.message_url = f'https://{contract}msging.net/messages'
     
     def request_identity(self, server_identity:str):
         payload = {
